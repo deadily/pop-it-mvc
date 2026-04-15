9e56f9b42b89a81e190ccbedf1fcadc2342bf74b
dara <zuevadara04@gmail.com>
2026-04-15 18:17:43 +0700

    fixed fully

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'deadily/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '07ab99c0b77a8ce0d1d139c4a5e3322411a9bd48',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'deadily/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '07ab99c0b77a8ce0d1d139c4a5e3322411a9bd48',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
